2948a0ff10514447ee05bce39c61b8a8d78fd3ac
sherifa-aljutaili <142884985+sherifa-aljutaili@users.noreply.github.com>
2023-09-16 12:12:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# write your code here
+person = {
+"name" : "sherifa",
+"age" : 18,
+"hobbies" : ["swimming", "reading"]
+}
+
+
+
+
+print(person['name'])
+print(person['age'])
+person["age"] = 23
+person["country"] = "kuwait"
+print(person)
+print(f"number of objects in dictionary: {len((person))}")
+person["hobbies"].append("codeing")
+def check_hobbies(person) :
+    if len(person["hobbies"]) >= 3 :
+        return print("wow you are amazing")
+    else :
+        return print("keep exploring your hobbies")
+    
+check_hobbies(person)
